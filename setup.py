b48978b865d798ee0e55ad50aaafecb3297045eb
gabriel-dahan <gabrieldahan05@gmail.com>
2021-04-05 20:00:47 +0200

    Added setup.py file for install support.

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = "dpy_cooldowns",
+    version = "1.0",
+    author = "Gabriel D.",
+    description = "A package allowing you to create database command cooldowns w/discord.py.",
+    url = "https://github.com/gabriel-dahan/dpy-cooldowns",
+    packages = setuptools.find_packages(),
+    python_requires = '>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
